b947b995a5ef7789c09175132dad12db018c66d1
sumit patil <sumitpatil90990@gmail.com>
2021-12-06 18:58:17 +0530

    added form input for login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,47 @@
+import { useState } from "react";
 import {Link} from "react-router-dom";
 
 export default function Login(){
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault();
+        console.log("login", formData);
+        setFormData({
+            email:"",
+            password:""
+        })
+    }
+
     return (
         <div className="component">
             <section className="pt-32 login flex justify-center">
                 <div className="w-full max-w-xs">
-                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleFormSubmit}>
                         <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                             Email
                         </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Email"></input>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Email" name="email" value={formData.email} onChange={handleChange}></input>
                         </div>
                         <div className="mb-6">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                             Password
                         </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="*****" name="password" value={formData.password} onChange={handleChange}></input>
                         </div>
                         <div className="flex items-center justify-between">
-                        <button className="px-6 py-2 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600" type="button">
+                        <button className="px-6 py-2 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600" type="submit">
                             Sign In
                         </button>
                         <Link to="/register">
